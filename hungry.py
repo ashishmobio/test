4d3487064349913e14137cd76192127ce928b99a
Ashish Patel <ashish.patel@mobiosolutions.com>
2022-10-08 14:38:01 +0530

    added sugar

--- hungry.py
+++ hungry.py
@@ -2,5 +2,6 @@ hungry = input("Are you hungry?")
 if hungry == "yes":
     print("Eat samosa.")
     print("Eat carrots for good.")
+    print("Eat too much sugar.")
 else:
     print("Do your homework.")
