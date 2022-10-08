1235f2f96cc51d3ade2627c3b107342c0bb91c93
Ashish Patel <ashish.patel@mobiosolutions.com>
2022-10-08 14:39:24 +0530

    Revert "added sugar"
    This reverts commit 4d3487064349913e14137cd76192127ce928b99a.

--- hungry.py
+++ hungry.py
@@ -2,6 +2,5 @@ hungry = input("Are you hungry?")
 if hungry == "yes":
     print("Eat samosa.")
     print("Eat carrots for good.")
-    print("Eat too much sugar.")
 else:
     print("Do your homework.")
